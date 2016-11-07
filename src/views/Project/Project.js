a25bc205e2037699f908e854d04db85f93ebbbc9
Joakim Granbohm <joakim.granbohm@sysart.fi>
2016-11-07 13:09:48 +0200

    fix bad marge

--- src/views/Project/Project.js
+++ src/views/Project/Project.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 
-<<<<<<< HEAD
 import Image from 'components/Image/Image';
 
-=======
->>>>>>> master
 class Project extends Component {
 
     render() {
         return (
             <div className="project-view">
-<<<<<<< HEAD
                 <div className="project-view__header">
                     <h2>Project {this.props.params.projectId}</h2>
                 </div>
@@ -18,9 +14,6 @@ class Project extends Component {
                 <div className="project__images">
                     <Image></Image>
                 </div>
-=======
-                <h2>Project {this.props.params.projectId}</h2>
->>>>>>> master
             </div>
         );
     }
