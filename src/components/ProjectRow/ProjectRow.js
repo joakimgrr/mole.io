a341618623963e28f57a41240f66fa190131abdf
Joakim Granbohm <joakim.granbohm@sysart.fi>
2016-11-07 13:28:24 +0200

    project page: initial layout with status indicator and image

--- src/components/ProjectRow/ProjectRow.js
+++ src/components/ProjectRow/ProjectRow.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
+import StatusIndicator from 'components/StatusIndicator/StatusIndicator';
+
 import './projectRow.scss';
 
 class ProjectRow extends Component {
@@ -12,7 +14,9 @@ class ProjectRow extends Component {
                         <h3 className="project-row__name">{this.props.project.name}</h3>
                         <span>{this.props.project.description}</span>
                     </div>
-                    <StatusIndicator status={this.props.project.status}></StatusIndicator>
+                    <div className="project-row__status-wrapper">
+                        <StatusIndicator status={this.props.project.status}></StatusIndicator>
+                    </div>
                 </div>
             </Link>
         );
--- src/views/Project/Project.js
+++ src/views/Project/Project.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 import Image from 'components/Image/Image';
+import StatusIndicator from 'components/StatusIndicator/StatusIndicator';
+
+import './project.scss';
 
 class Project extends Component {
 
@@ -8,7 +11,12 @@ class Project extends Component {
         return (
             <div className="project-view">
                 <div className="project-view__header">
-                    <h2>Project {this.props.params.projectId}</h2>
+                    <div className="project-view__title-wrapper">
+                        <h2>Project {this.props.params.projectId}</h2>
+                    </div>
+                    <div className="project-view__status-wrapper">
+                        <StatusIndicator status={'passed'}></StatusIndicator>
+                    </div>
                 </div>
 
                 <div className="project__images">
