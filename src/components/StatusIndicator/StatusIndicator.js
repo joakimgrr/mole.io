14fb0294c623804b897be48aeb9db7b350f54cd9
Joakim Granbohm <joakim.granbohm@sysart.fi>
2016-11-08 13:16:27 +0200

    refactor statusindicator component for clarity

--- src/components/StatusIndicator/StatusIndicator.js
+++ src/components/StatusIndicator/StatusIndicator.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './statusIndicator.scss';
 
-class StatusIndicator extends Component {
-    render() {
-        return (
-            <div className={'status-indicator status-indicator--' + this.props.status}>
-                <span className="status-indicator__statustext">{this.props.status}</span>
-            </div>
-        )
-    }
-}
+const StatusIndicator = ({status}) => (
+    <div className={'status-indicator status-indicator--' + status}>
+        <span className="status-indicator__statustext">{status}</span>
+    </div>
+)
 
 StatusIndicator.PropTypes = {
     status: React.PropTypes.string.isRequired
